a1656a7c6493a66219ee2948ee0950ef61e54826
Yanis-Ourir <ourir.yanis@gmail.com>
2023-04-24 14:33:04 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Welcome from './components/Welcome';
+import image from './images/Dio.png';
+
 
 function App() {
+  console.log(<Welcome />);
+  const [count, setCount] = useState(0);
+  const [seconds, setTime] = useState(0);
+  const [active, setActive] = useState(false);
+
+  function addOne() {
+    setCount(count + 1);
+  }
+  function retireOne() {
+    setCount(count - 1);
+  }
+
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('a second has passed');
+      setTime(seconds + 1);
+    }, 1000);
+    if (seconds === 10) {
+      clearInterval(interval);
+      setActive(true);
+    }
+    return () => clearInterval(interval);
+  });
+
+  function sayAnything() {
+    if (seconds === 10) {
+      return <h1 style={{ fontSize: 50 }} class="center-warudo">ZA WARUDO !!!</h1>;
+    }
+
+  }
+
+  function dio() {
+    if (seconds === 10) {
+      return <img src={image} alt="" class="dio"></img>;
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tkt" style={{ backgroundColor: active ? 'black' : 'white', color: active ? 'white' : 'black' }}>
+      <section>
+        <Welcome firstname='Yanis' lastname='Ourir' />
+        <p>Vous avez cliqué : {count} fois !</p>
+        <button onClick={addOne}>Ajouter 1</button>
+        <button onClick={retireOne}>Retirer 1</button>
+      </section>
+
+      <section>
+        <p>{seconds}s s'est écoulé</p>
+        {sayAnything()}
+        <div class='center-dio'>
+          {dio()}
+        </div>
+
+      </section>
     </div>
   );
 }
